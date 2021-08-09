ab5f13ce731464919ef4d31a35f41a4cba0458fa
Yousef-Ahmad1997128 <yousef.alsharif1997@gmail.com>
2021-08-09 11:33:03 +0300

    changed the function so that it will show the initial value of the product Relates #46

--- details/details.js
+++ details/details.js
@@ -6,24 +6,24 @@ let total = document.getElementById('totalPrice');
 
 let int = document.getElementById('amount');
 let number = 1;
-let priceTotal = parseInt(price.innerHTML);
+let priceTotal = parseInt(price.textContent);
+total.textContent = price.textContent
+console.log(price.textContent)
 
 
 add.addEventListener('click', () =>{
     number += 1;
-    priceTotal += parseInt(price.innerHTML);
-    int.innerHTML = number;
-    total.innerHTML = priceTotal;
+    priceTotal += parseInt(price.textContent);
+    int.textContent = number;
+    total.textContent = priceTotal;
 })
 
 remove.addEventListener('click', () =>{
-    if(number > 0 ){
+    if(number > 1 ){
         number -=1;
-        priceTotal -= parseInt(price.innerHTML);
-        int.innerHTML = number;
-        total.innerHTML = priceTotal;
+        priceTotal -= parseInt(price.textContent);
+        int.textContent = number;
+        total.textContent = priceTotal;
     }
-    else {
-        alert('cannot add zero Items');
-    }
+   
 })
