3580a382a116b39f43650bb72f39ecbf15b0b4ac
MohmmedHasan-99 <mo7ammed.mah@gmail.com>
2021-08-09 12:14:57 +0300

    make the login function outline, check the input  email and password if they match any user data relates #57

---
+++ login/login.js
@@ -0,0 +1,22 @@
+
+let loginBtn = document.getElementsByClassName("login-btn")[0];
+let users = JSON.parse(localStorage.getItem('users'));
+
+loginBtn.addEventListener('click', logIn);
+
+
+function logIn(){
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+    let message = document.getElementById("incorrctMsg");
+
+    let existUser = false;
+    for(let i=0;i<users.length;i++){
+        if(email == users[i].Email){
+            if(password == users[i].Password)
+            existUser = true;
+        }
+    }
+    if(!existUser) message.setAttribute('style','display: block; color: red;');
+}
+// onclick="window.lgit ocation.href='../home/home.html';"
