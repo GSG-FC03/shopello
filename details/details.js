4f97da511faced3fcb345459b89193300bb02d06
Yousef-Ahmad1997128 <yousef.alsharif1997@gmail.com>
2021-08-09 12:14:07 +0300

    added a function that gets the take the product information from the data object and deisplay it on thedetails page Relates #46

--- details/details.js
+++ details/details.js
@@ -2,15 +2,19 @@ let add = document.getElementById('plus');
 let remove = document.getElementById('minus');
 let price = document.getElementById('originalPice');
 let total = document.getElementById('totalPrice');
-
-
+let title = document.getElementById('productTitle')
+let description = document.getElementById('productDesc')
+let category = document.getElementById('productCategory')
+let image = document.getElementById('productCategory')
 let int = document.getElementById('amount');
+
+
 let number = 1;
 let priceTotal = parseInt(price.textContent);
 total.textContent = price.textContent
-console.log(price.textContent)
 
 
+// to add an item.
 add.addEventListener('click', () =>{
     number += 1;
     priceTotal += parseInt(price.textContent);
@@ -18,6 +22,7 @@ add.addEventListener('click', () =>{
     total.textContent = priceTotal;
 })
 
+// to remove an item.
 remove.addEventListener('click', () =>{
     if(number > 1 ){
         number -=1;
@@ -27,3 +32,14 @@ remove.addEventListener('click', () =>{
     }
    
 })
+
+
+// to get the producat information and display iton the page.
+function displayDetails(data) {
+    image.src = data.image
+    category.textContent = data.category
+    title.textContent = data.title 
+    description.textContent = data.description
+    price.textContent = data.price 
+}
+
