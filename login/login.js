17c8ef6fba42fc269f9d3778743eb5d567dfe3b1
MohmmedHasan-99 <mo7ammed.mah@gmail.com>
2021-08-09 14:48:51 +0300

    fix login bug is some case relates #57

--- login/login.js
+++ login/login.js
@@ -3,29 +3,41 @@ let loginBtn = document.getElementsByClassName("login-btn")[0];
 let users = JSON.parse(localStorage.getItem('users'));
 
 loginBtn.addEventListener('click', logIn);
+let arrayOfUser = [];
+if (localStorage.getItem("users") == null) {
+    localStorage.setItem("users", JSON.stringify(arrayOfUser))
+}
 
 
+let stupid = false;
 function logIn(){
+    
     let email = document.getElementById("email").value;
     let password = document.getElementById("password").value;
     let message = document.getElementById("incorrctMsg");
 
     let existUser = false;
     for(let i=0;i<users.length;i++){
-        if(email == users[i].Email){
-            if(password == users[i].Password){
-                existUser = true;
-                users[i] = mergeProduct(users[i]);
-                localStorage.setItem('users', JSON.stringify(users));
-                break;
-            }
+        if(email == users[i].Email && password == users[i].Password){
+            existUser = true;
+            users[i] = mergeProduct(users[i]);
+            localStorage.setItem('users', JSON.stringify(users));
+            break;
         }
     }
-    if(!existUser) message.setAttribute('style','display: block; color: red;');
-    else
-        window.location.href='../home/home.html';
+    
+    if(!existUser){
+        if(email == "" || password =="")
+        message.textContent = "Please fill empty fields";
+        else
+        message.textContent = "Incorrect email or password";
+        message.setAttribute('style','display: block; color: red;');   
+    } 
+    
+    else{
+        window.location.href='../home/home.html';  
+    }
 }
-
 
 function mergeProduct(user){
     let unknown = JSON.parse(localStorage.getItem("unknown"));
