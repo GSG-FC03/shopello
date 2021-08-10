f3e4b74a9b73c70493e0d339d631d79f0c76b34c
Yousef-Ahmad1997128 <yousef.alsharif1997@gmail.com>
2021-08-10 12:26:57 +0300

    added the buy message after the user presses the buy button the mwessage will be displayed. Relates #79

--- details/details.js
+++ details/details.js
@@ -9,12 +9,65 @@ let image = document.getElementById('productImage')
 let int = document.getElementById('amount');
 let buy = document.getElementById('buy');
 let addCart = document.getElementById('add');
-let currency = document.getElementById('priceTag');
+let currencySymbol = document.getElementById('priceTag');
+let buyMsg = document.getElementById('buyMsg');
 
+let unknown = localStorage.getItem("unknown") === null ? [] : JSON.parse(localStorage.getItem("unknown"));
+
+
+
+
+//fetch currencies and exchange prices 
+unknown.Currency = 'EUR'
+let Currency,
+  rate = 1,
+  symbol = '$';
+if (unknown.Currency == '') Currency = 'USD'
+else {
+  Currency = unknown.Currency
+  symbol = Currency
+  let from = "USD",
+    to = Currency;
+
+  (async function getData() {
+    try {
+      const response = await fetch(`https://currency-exchange.p.rapidapi.com/exchange?to=${to}&from=${from}&q=1.0`, {
+        "method": "GET",
+        "headers": {
+          "x-rapidapi-key": "6155594c89msh387173eac4635c0p108063jsnee1a9a20e441",
+          "x-rapidapi-host": "currency-exchange.p.rapidapi.com"
+        }
+      })
+      const data = await response.json()
+      rate = data;
+      console.log(rate)
+    } catch (e) {
+      console.log("error", e.message)
+    }
+  })()
+}
+
+
+
+let priceTotal
+
+// to get the producat information and display iton the page.
+let data = localStorage.getItem("data") === null ? [] : JSON.parse(localStorage.getItem("data"));
+console.log(data)
+function displayDetails(data) {
+    currencySymbol.textContent = symbol
+    image.setAttribute('src', `${data.image}`)
+    category.textContent = data.category
+    title.textContent = data.title 
+    description.textContent = data.description
+    let exchangePrice = data.price * rate 
+    price.textContent = exchangePrice.toFixed(2) 
+    priceTotal = parseInt(price.textContent);
+    console.log(price.textContent)
+    total.textContent = price.textContent
+}
 
 let number = 1;
-let priceTotal = parseInt(price.textContent);
-total.textContent = price.textContent
 
 
 // to add an item.
@@ -36,21 +89,51 @@ remove.addEventListener('click', () =>{
    
 })
 
+//add the product into object and storage it into loacal storage
+function addToCart(data) {
+    unknown = localStorage.getItem("unknown") === null ? [] : JSON.parse(localStorage.getItem("unknown"));
+  
+    let newProduct = {
+      id: "",
+      title: "",
+      description: "",
+      category: "",
+      image: "",
+      price: "",
+      quantity: ""
+    }
+  
+    newProduct.id = data.id
+    newProduct.title = data.title
+    newProduct.description = data.description
+    newProduct.category = data.category
+    newProduct.image = data.image
+    newProduct.price = data.price
+    newProduct.quantity = 1
+  
+    if (unknown.Product.length == 0) {
+      unknown.Product.push(newProduct)
+    } else {
+      let flag = false
+      for (let index = 0; index < unknown.Product.length; index++) {
+        if (unknown.Product[index].id == newProduct.id) {
+          unknown.Product[index].quantity += 1;
+          flag = false
+          break;
+        } else {
+          flag = true
+        }
+      }
+      if (flag) unknown.Product.push(newProduct)
+    }
+    localStorage.setItem("unknown", JSON.stringify(unknown));
+  }
+  
 
-// to get the producat information and display iton the page.
-let data = localStorage.getItem("data") === null ? [] : JSON.parse(localStorage.getItem("data"));
-console.log(data)
-displayDetails(data)
-function displayDetails(data) {
-    image.setAttribute('src', `${data.image}`)
-    category.textContent = data.category
-    title.textContent = data.title 
-    description.textContent = data.description
-    price.textContent = data.price 
-}
 
 
 addCart.addEventListener('click', ()=>{
+    addToCart(data)
     location.href = '../cart/cart.html'
 })
 
@@ -61,9 +144,12 @@ buy.addEventListener('click', ()=>{
         window.location.href = '../createAccount/createAccount.html'
     }
     else
-    console.log('ho')
-    window.location.href = '#'
+    buyMsg.style.display = 'block'
 })
+
+setTimeout(() =>{
+    displayDetails(data)
+}, 1000)
 
 
 
