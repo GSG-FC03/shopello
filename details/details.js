670ef2c240a3711b611af3f34df56af45a8f833b
MohammadAlHabil <mohammadalhabil1999@gmail.com>
2021-08-11 11:50:10 +0300

    fix the error and modify the price show >relates #86

--- details/details.js
+++ details/details.js
@@ -12,20 +12,18 @@ let addCart = document.getElementById('add');
 let currencySymbol = document.getElementById('priceTag');
 let buyMsg = document.getElementById('buyMsg');
 
-
 let unknown = localStorage.getItem("unknown") === null ? [] : JSON.parse(localStorage.getItem("unknown"));
 
-
-
 //fetch currencies and exchange prices 
-unknown.Currency = 'USD'
-
-
 let Currency,
   rate = 1,
-  symbol = '$';
-if (unknown.Currency == '') Currency = 'USD'
-else {
+  symbol = '$',
+  timer;
+if (unknown.Currency == '') {
+  Currency = 'USD'
+  timer = 0
+} else {
+  timer = 300
   Currency = unknown.Currency
   symbol = Currency
   let from = "USD",
@@ -42,7 +40,6 @@ else {
       })
       const data = await response.json()
       rate = data;
-      console.log(rate)
     } catch (e) {
       console.log("error", e.message)
     }
@@ -51,146 +48,98 @@ else {
 
 
 let priceTotal
-
-// to get the producat information and display iton the page.
-let data = localStorage.getItem("data") === null ? [] : JSON.parse(localStorage.getItem("data"));
-console.log(data)
-function displayDetails(data) {
-    currencySymbol.textContent = symbol
-    image.setAttribute('src', `${data.image}`)
-    category.textContent = data.category
-    title.textContent = data.title 
-    description.textContent = data.description
-    let exchangePrice = data.price * rate 
-    price.textContent = exchangePrice.toFixed(2) 
-    priceTotal = parseInt(price.textContent);
-    console.log(price.textContent)
-    total.textContent = price.textContent
-}
-
-
-
-let priceTotal
 let currencyDiv = document.createElement('div')
-
 // to get the producat information and display iton the page.
 let data = localStorage.getItem("data") === null ? [] : JSON.parse(localStorage.getItem("data"));
-console.log(data)
+
 function displayDetails(data) {
-    currencySymbol.textContent = symbol
-    image.setAttribute('src', `${data.image}`)
-    category.textContent = data.category
-    title.textContent = data.title 
-    description.textContent = data.description
-    let exchangePrice = data.price * rate 
-    price.textContent = exchangePrice.toFixed(2) 
-    priceTotal = parseInt(price.textContent);
-    console.log(price.textContent)
-    total.textContent = price.textContent
-    currencyDiv.setAttribute('id', 'priceTag')
-    currencyDiv.textContent = currencySymbol.textContent
-    total.appendChild(currencyDiv)
-
+  currencySymbol.textContent = symbol
+  image.setAttribute('src', `${data.image}`)
+  category.textContent = data.category
+  title.textContent = data.title
+  description.textContent = data.description
+  let exchangePrice = data.price * rate
+  price.textContent = exchangePrice.toFixed(2)
+  priceTotal = parseFloat(price.textContent);
+  currencyDiv.setAttribute('id', 'priceTag')
+  currencyDiv.textContent = currencySymbol.textContent
+  total.textContent = price.textContent
+  total.insertBefore(currencyDiv,total.childNodes[0])
 }
-
-
 
 let number = 1;
-
 // to add an item.
-add.addEventListener('click', () =>{
-    number += 1;
-    priceTotal += parseInt(price.textContent);
-    int.textContent = number;
-    total.textContent = priceTotal;
-    currencyDiv.setAttribute('id', 'priceTag')
-    currencyDiv.textContent = currencySymbol.textContent
-    total.appendChild(currencyDiv)
+add.addEventListener('click', () => {
+  number += 1;
+  priceTotal += parseFloat(price.textContent);
+  int.textContent = number;
+  total.childNodes[1].textContent = priceTotal.toFixed(2);
 })
 
 // to remove an item.
-remove.addEventListener('click', () =>{
-    if(number > 1 ){
-        number -=1;
-        priceTotal -= parseInt(price.textContent);
-        int.textContent = number;
-        total.textContent = priceTotal;
-        currencyDiv.setAttribute('id', 'priceTag')
-        currencyDiv.textContent = currencySymbol.textContent
-        total.appendChild(currencyDiv)
-    }
-   
+remove.addEventListener('click', () => {
+  if (number > 1) {
+    number -= 1;
+    priceTotal -= parseFloat(price.textContent);
+    int.textContent = number;
+    total.childNodes[1].textContent = priceTotal.toFixed(2);
+  }
+
 })
 
 //add the product into object and storage it into loacal storage
 function addToCart(data) {
-    unknown = localStorage.getItem("unknown") === null ? [] : JSON.parse(localStorage.getItem("unknown"));
-  
-    let newProduct = {
-      id: "",
-      title: "",
-      description: "",
-      category: "",
-      image: "",
-      price: "",
-      quantity: ""
-    }
-  
-    newProduct.id = data.id
-    newProduct.title = data.title
-    newProduct.description = data.description
-    newProduct.category = data.category
-    newProduct.image = data.image
-    newProduct.price = data.price
-    newProduct.quantity = 1
-  
-    if (unknown.Product.length == 0) {
-      unknown.Product.push(newProduct)
-    } else {
-      let flag = false
-      for (let index = 0; index < unknown.Product.length; index++) {
-        if (unknown.Product[index].id == newProduct.id) {
-          unknown.Product[index].quantity += 1;
-          flag = false
-          break;
-        } else {
-          flag = true
-        }
-      }
-      if (flag) unknown.Product.push(newProduct)
-    }
-    localStorage.setItem("unknown", JSON.stringify(unknown));
+  unknown = localStorage.getItem("unknown") === null ? [] : JSON.parse(localStorage.getItem("unknown"));
+
+  let newProduct = {
+    id: "",
+    title: "",
+    description: "",
+    category: "",
+    image: "",
+    price: "",
+    quantity: ""
   }
-  
 
+  newProduct.id = data.id
+  newProduct.title = data.title
+  newProduct.description = data.description
+  newProduct.category = data.category
+  newProduct.image = data.image
+  newProduct.price = data.price
+  newProduct.quantity = 1
 
-
-addCart.addEventListener('click', ()=>{
-    addToCart(data)
-    location.href = '../cart/cart.html'
-})
-
-
-buy.addEventListener('click', ()=>{
-    let unknown = localStorage.getItem("unknown") === null ? [] : JSON.parse(localStorage.getItem("unknown"));
-    if(unknown.Name === ''){
-        window.location.href = '../createAccount/createAccount.html'
+  if (unknown.Product.length == 0) {
+    unknown.Product.push(newProduct)
+  } else {
+    let flag = false
+    for (let index = 0; index < unknown.Product.length; index++) {
+      if (unknown.Product[index].id == newProduct.id) {
+        unknown.Product[index].quantity += number;
+        flag = false
+        break;
+      } else {
+        flag = true
+      }
     }
-    else
+    if (flag) unknown.Product.push(newProduct)
+  }
+  localStorage.setItem("unknown", JSON.stringify(unknown));
+}
 
-    buyMsg.style.display = 'block'
-
+addCart.addEventListener('click', () => {
+  addToCart(data)
+  location.href = '../cart/cart.html'
 })
 
-setTimeout(() =>{
-    displayDetails(data)
-}, 500)
-=======
+buy.addEventListener('click', () => {
+  let unknown = localStorage.getItem("unknown") === null ? [] : JSON.parse(localStorage.getItem("unknown"));
+  if (unknown.Name === '') {
+    window.location.href = '../createAccount/createAccount.html'
+  } else
+    buyMsg.style.display = 'block'
+})
 
-
-
-
-
-
-
-
+setTimeout(() => {
+  displayDetails(data)
+}, timer)
